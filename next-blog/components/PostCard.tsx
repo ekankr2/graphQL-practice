94fd953cb5153a7cf2ef48a585481ec2bdc07b32
ekankr2 <ekankr2@naver.com>
2022-06-13 19:31:02 +0900

    post title

--- next-blog/components/PostCard.tsx
+++ next-blog/components/PostCard.tsx
@@ -1,7 +1,8 @@
 import React, {FC} from 'react';
+import Link from "next/link";
 
-interface Props{
-    post: Post
+interface Props {
+    post: any
 }
 
 const PostCard: FC<Props> = ({post}) => {
@@ -10,12 +11,18 @@ const PostCard: FC<Props> = ({post}) => {
     return (
         <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
             <div className="relative overflow-hidden shadow-md pb-80 mb-6">
-                {/*<img*/}
-                {/*    src={post.featuredImage.url}*/}
-                {/*    alt={post.title}*/}
-                {/*    className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"*/}
-                {/*    />*/}
+                <img
+                    src={post.featuredImage.url}
+                    alt={post.title}
+                    className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"
+                />
             </div>
+            <h1 className="transition duration-700 text-center mb-8 cursor-pointer
+            hover:text-pink-600 text-3xl font-semibold">
+                <Link href={`/post/${post.slug}`}>
+                    {post.title}
+                </Link>
+            </h1>
         </div>
     );
 };
