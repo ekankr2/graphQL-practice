9ab9d2b6ae3ae0d3e9aa97c3a55e60cdd761e704
ekankr2 <ekankr2@naver.com>
2022-06-14 19:36:54 +0900

    post widget

--- next-blog/components/PostWidget.tsx
+++ next-blog/components/PostWidget.tsx
@@ -1,27 +1,48 @@
-import React, {FC, useEffect, useState} from 'react';
-import {getRecentPosts, getSimilarPosts} from "../services";
+import React, {useState, useEffect, FC} from 'react';
+import moment from 'moment';
+import Link from 'next/link';
+import { getSimilarPosts, getRecentPosts } from '../services';
 
 interface Props {
     categories: string[]
     slug: string
 }
 
-const PostWidget:FC<Props> = ({ categories, slug}) => {
-    const [relatedPosts, setRelatedPosts] = useState([])
+const PostWidget: FC<Props> = ({ categories, slug }) => {
+    const [relatedPosts, setRelatedPosts] = useState([]);
 
     useEffect(() => {
-        if(slug) {
-            getSimilarPosts(categories, slug)
-                .then((result) => setRelatedPosts(result))
+        if (slug) {
+            getSimilarPosts(categories, slug).then((result) => {
+                setRelatedPosts(result);
+            });
         } else {
-            getRecentPosts()
-                .then((result) => setRelatedPosts(result))
+            getRecentPosts().then((result) => {
+                setRelatedPosts(result);
+            });
         }
-    },[input])
+    }, [slug]);
 
     return (
-        <div>
-            PostWidget
+        <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+            <h3 className="text-xl mb-8 font-semibold border-b pb-4">{slug ? 'Related Posts' : 'Recent Posts'}</h3>
+            {relatedPosts.map((post, index) => (
+                <div key={index} className="flex items-center w-full mb-4">
+                    <div className="w-16 flex-none">
+                        <img
+                            alt={post.title}
+                            height="60px"
+                            width="60px"
+                            className="align-middle rounded-full"
+                            src={post.featuredImage.url}
+                        />
+                    </div>
+                    <div className="flex-grow ml-4">
+                        <p className="text-gray-500 font-xs">{moment(post.createdAt).format('MMM DD, YYYY')}</p>
+                        <Link href={`/post/${post.slug}`} className="text-md" key={index}>{post.title}</Link>
+                    </div>
+                </div>
+            ))}
         </div>
     );
 };
--- next-blog/types.d.ts
+++ next-blog/types.d.ts
@@ -1,6 +1,8 @@
 interface Post {
     title: string
+    slug: string
     featuredImage: {
         url: string
     }
+    createdAt: any
 }
