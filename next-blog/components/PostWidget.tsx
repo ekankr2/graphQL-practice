f578da4942a57399f61cd51cd825aca803d7a081
ekankr2 <ekankr2@naver.com>
2022-06-14 19:25:13 +0900

    related post services

--- next-blog/components/PostWidget.tsx
+++ next-blog/components/PostWidget.tsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, {FC, useEffect, useState} from 'react';
+import {getRecentPosts, getSimilarPosts} from "../services";
 
-const PostWidget = () => {
+interface Props {
+    categories: string[]
+    slug: string
+}
+
+const PostWidget:FC<Props> = ({ categories, slug}) => {
+    const [relatedPosts, setRelatedPosts] = useState([])
+
+    useEffect(() => {
+        if(slug) {
+            getSimilarPosts(categories, slug)
+                .then((result) => setRelatedPosts(result))
+        } else {
+            getRecentPosts()
+                .then((result) => setRelatedPosts(result))
+        }
+    },[input])
+
     return (
         <div>
             PostWidget
