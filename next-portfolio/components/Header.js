b7c37f735f3fedbcde49d159e9dca643440c12ce
ekankr2 <ekankr2@naver.com>
2022-06-27 19:49:46 +0900

    useTheme

--- next-portfolio/components/Header.js
+++ next-portfolio/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from "next/link";
+import DarkModeToggleButton from "./dark-mode-toggle-button";
 
 function Header(props) {
     return (
@@ -25,13 +26,7 @@ function Header(props) {
                         </Link>
                         <a href="https://github.com/ekankr2" className="mr-5 hover:text-gray-900">연락하기</a>
                     </nav>
-                    <button
-                        className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
-                        <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
-                             strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
-                            <path d="M5 12h14M12 5l7 7-7 7"></path>
-                        </svg>
-                    </button>
+                    <DarkModeToggleButton/>
                 </div>
             </header>
         </>
