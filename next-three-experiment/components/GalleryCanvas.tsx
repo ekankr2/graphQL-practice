d035bdcb873c29dca7a9adcb18ea71099170e592
ekankr2 <ekankr2@naver.com>
2022-08-19 12:07:22 +0900

    gallery angle fix

--- next-three-experiment/components/GalleryCanvas.tsx
+++ next-three-experiment/components/GalleryCanvas.tsx
@@ -6,21 +6,38 @@ import {useLocation, useRoute} from "wouter";
 import getUuid from 'uuid-by-string'
 
 const pexel = (id: number) => `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`
+
 const images = [
     // Front
-    {position: [0, 0, 1.5], rotation: [0, 0, 0], url: pexel(1103970)},
+    {position: [-3.5, 0, 2.5], rotation: [0, 0, 0], url: pexel(1103970)},
     // Back
-    {position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(416430)},
-    {position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(310452)},
+    {position: [-2.4, 0, 2.55], rotation: [0, -Math.PI / 12, 0], url: pexel(416430)},
+    {position: [-1.65, 0, 2.6], rotation: [0, -Math.PI / 8.5, 0], url: pexel(310452)},
     // Left
-    {position: [-1.75, 0, 0.25], rotation: [0, Math.PI / 2.5, 0], url: pexel(327482)},
-    {position: [-2.15, 0, 1.5], rotation: [0, Math.PI / 2.5, 0], url: pexel(325185)},
-    {position: [-2, 0, 2.75], rotation: [0, Math.PI / 2.5, 0], url: pexel(358574)},
+    {position: [-1, 0, 2.55], rotation: [0, -Math.PI / 7, 0], url: pexel(327482)},
+    {position: [-0.3, 0, 2.5], rotation: [0, -Math.PI / 5.5, 0], url: pexel(325185)},
+    {position: [0.5, 0, 2.5], rotation: [0, -Math.PI / 4.3, 0], url: pexel(358574)},
     // Right
-    {position: [1.75, 0, 0.25], rotation: [0, -Math.PI / 2.5, 0], url: pexel(227675)},
-    {position: [2.15, 0, 1.5], rotation: [0, -Math.PI / 2.5, 0], url: pexel(911738)},
-    {position: [2, 0, 2.75], rotation: [0, -Math.PI / 2.5, 0], url: pexel(1738986)}
+    {position: [1.4, 0, 2.55], rotation: [0, -Math.PI / 4, 0], url: pexel(227675)},
+    {position: [2.2, 0, 2.6], rotation: [0, -Math.PI / 3.6, 0], url: pexel(911738)},
+    {position: [3, 0, 2.75], rotation: [0, -Math.PI / 3.5, 0], url: pexel(1738986)}
 ]
+
+// const images = [
+//     // Front
+//     {position: [0, 0, 1.5], rotation: [0, 0, 0], url: pexel(1103970)},
+//     // Back
+//     {position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(416430)},
+//     {position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(310452)},
+//     // Left
+//     {position: [-1.75, 0, 0.25], rotation: [0, Math.PI / 2.5, 0], url: pexel(327482)},
+//     {position: [-2.15, 0, 1.5], rotation: [0, Math.PI / 2.5, 0], url: pexel(325185)},
+//     {position: [-2, 0, 2.75], rotation: [0, Math.PI / 2.5, 0], url: pexel(358574)},
+//     // Right
+//     {position: [1.75, 0, 0.25], rotation: [0, -Math.PI / 2.5, 0], url: pexel(227675)},
+//     {position: [2.15, 0, 1.5], rotation: [0, -Math.PI / 2.5, 0], url: pexel(911738)},
+//     {position: [2, 0, 2.75], rotation: [0, -Math.PI / 2.5, 0], url: pexel(1738986)}
+// ]
 
 const GOLDENRATIO = 1.61803398875
 
