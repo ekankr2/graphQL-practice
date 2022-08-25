6e4e862004337b221052a65533707d9f17c6bba8
ekankr2 <ekankr2@naver.com>
2022-08-25 20:23:07 +0900

    carousel

--- next-three-experiment/pages/_app.tsx
+++ next-three-experiment/pages/_app.tsx
@@ -2,6 +2,8 @@ import '../styles/globals.css'
 import '../styles/main.scss';
 import type { AppProps } from 'next/app'
 
+import '../styles/carousel.css'
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
--- next-three-experiment/pages/carousel.tsx
+++ next-three-experiment/pages/carousel.tsx
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import {NextPage} from "next";
+import CarouselSlider from "../components/CarouselSlider";
 
-const Carousel:NextPage = () => {
+
+const items = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+];
+
+const Carousel: NextPage = () => {
+    const [index, setIndex] = useState(0);
+    const [disabled, setDisabled] = useState(false);
+
+    const onIndexChange = useCallback((nextIndex, isLast) => {
+        setIndex(nextIndex);
+        setDisabled(isLast);
+    }, []);
+
+    const onNextClick = useCallback(() => {
+        setIndex(index + 1);
+    }, [setIndex, index]);
+
+    const onPrevClick = useCallback(() => {
+        setIndex(index - 1);
+    }, [setIndex, index]);
+
     return (
-        <></>
+        <div className="flex">
+            <CarouselSlider onIndexChange={onIndexChange}
+                            index={index}
+                            containerClassName="Slider_With_padding"
+                            items={items}
+                            padding={37.5}>
+                {(item, key) => (
+                    <div key={key} className="Slider__Item">
+                        {item}
+                    </div>
+                )}
+            </CarouselSlider>
+        </div>
     );
 };
 
