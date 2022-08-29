3052dd2f6446d3888c971e0f8a19131301234b58
ekankr2 <ekankr2@naver.com>
2022-08-29 11:22:47 +0900

    y axis text carousel complete

--- next-three-experiment/pages/textCarousel.tsx
+++ next-three-experiment/pages/textCarousel.tsx
@@ -1,16 +1,19 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
 import {NextPage} from "next";
 import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
 
 const slides = [1,2,3,4,5]
 
 const TextCarousel: NextPage = () => {
-    const [viewportRef, embla] = useEmblaCarousel({ axis: "y", loop: true, align: 'center' });
+    const autoplay = useRef(
+        // @ts-ignore
+        Autoplay({ delay: 1500, stopOnInteraction: false }, (emblaRoot: any) => emblaRoot.parentElement)
+    );
+
+    const [viewportRef, embla] = useEmblaCarousel({ axis: "y", loop: true, align: 'center' }, [autoplay.current]);
     const [prevBtnEnabled, setPrevBtnEnabled] = useState(false);
     const [nextBtnEnabled, setNextBtnEnabled] = useState(false);
-
-    const scrollPrev = useCallback(() => embla?.scrollPrev(), [embla]);
-    const scrollNext = useCallback(() => embla?.scrollNext(), [embla]);
 
     const onSelect = useCallback(() => {
         if (!embla) return;
@@ -26,19 +29,21 @@ const TextCarousel: NextPage = () => {
     }, [embla, onSelect]);
 
     return (
-        <div className="bg-black text-white w-[300px] max-h-[500px]">
-            <div className="w-full overflow-hidden" ref={viewportRef}>
+        <div className="bg-black text-white max-w-[500px] max-h-[500px]">
+            <div className="w-full overflow-hidden relative" ref={viewportRef}>
                 <div className="h-[250px] flex flex-col select-none">
-                    {slides.map((index) => (
+                    {slides.map((data, index) => (
                         <div className="min-h-[33%] relative flex justify-center items-center" key={index}>
-                            <div className="overflow-hidden flex justify-center items-center">
-                                <h1 className="min-w-full min-h-full">
-                                    {index}
+                            <div className="overflow-hidden">
+                                <h1 className="min-w-full min-h-full font-bold text-2xl">
+                                    {data}
                                 </h1>
                             </div>
                         </div>
                     ))}
                 </div>
+                <div className="absolute w-full opacity-70 from-transparent via-black to-black h-[80px] bg-gradient-to-t top-0 left-0"></div>
+                <div className="absolute w-full opacity-70 bg-gradient-to-t from-black via-black to-transparent h-[80px] bottom-0 left-0"></div>
             </div>
         </div>
     );
